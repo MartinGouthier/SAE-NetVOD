49541b9f4efebb253da538993187ee7361e455f0
Yanis Husser <yanis.husser2@etu.univ-lorraine.fr>
2025-11-03 17:07:00 +0100

    modification de Serie

--- src/classes/video/Serie.php
+++ src/classes/video/Serie.php
@@ -1,6 +1,7 @@
 <?php
 namespace iutnc\netvod\video;
 class Serie {
+    private array $episodes;
     private string $title;
     private string $description;
     private string $cheminImage;
@@ -8,16 +9,21 @@ class Serie {
     private string $genre;
     private string $typePublic;
 
-    public function __construct(string $title, string $description, string $cheminImage,int $annee ,string $genre, string $typePublic) {
+    public function __construct(string $title, string $description, string $cheminImage,int $annee ,string $genre, string $typePublic, array  $episodes = []) {
         $this->title = $title;
         $this->description = $description;
         $this->cheminImage = $cheminImage;
         $this->annee = $annee;
         $this->genre = $genre;
         $this->typePublic = $typePublic;
+        $this->episodes = $episodes;
     }
 
     public function __get($name) {
         return $this->$name;
+    }
+
+    public function ajouterEpisode(EpisodeSerie $episode) : void{
+        $this->episodes[] = $episode;
     }
 }
