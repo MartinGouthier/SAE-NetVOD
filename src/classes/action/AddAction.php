c58e6ed03f0fea435fe50170f1a2cd8a73a3d767
delat-max <e43496u@etu.univ-lorraine.fr>
2025-11-04 15:18:35 +0100

    Modification temporaire de AddAction.php

--- src/classes/action/AddAction.php
+++ src/classes/action/AddAction.php
@@ -1,10 +1,36 @@
-    <?php
+<?php
 
 namespace iutnc\netvod\action;
 
-class AddAction extends Action{
-    public function execute() : string {
 
+
+use iutnc\netvod\repository\NetvodRepository;
+
+class AddAction extends Action{
+    public function GET(): string
+    {
+        return <<<END
+                <form method='post' action=?action=add><br>
+                Série : <input type='text' name='nom'><br>
+                <button type="submit">Ajouter à mes préférences</button> </form>
+        END;
+
+    }
+
+    public function POST(): string
+    {
+        $nom = $_POST['nom'];
+        $pl = new Liste($nom);
+
+        $pdo = NetvodRepository::getInstance();
+        $pdo->sauvegarderNouvelleListe($pl);
+        $pdo->saveUserListe(AuthnProvider::getSignedInUser(),$pl->__GET("id"));
+        $_SESSION['playlist'] = serialize($pl);
+        $html = <<<END
+             <b>Playlist créé en session</b><br>
+             <a href="?action=add-track">Ajouter à mes préférences</a>
+        END;
+        return $html;
     }
 
 }
