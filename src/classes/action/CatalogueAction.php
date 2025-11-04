fd18d33f89c6790e82699642d4f7fbc864dc38d1
DRVaurelio <direnzovilleraurelio@gmail.com>
2025-11-04 15:10:15 +0100

    --Affichage catalogue

--- src/classes/action/CatalogueAction.php
+++ src/classes/action/CatalogueAction.php
@@ -5,13 +5,30 @@ namespace iutnc\netvod\action;
 class CatalogueAction extends Action {
 
     public function GET(): string {
-        return <<<HTML
-            <h2>Catalogue des séries</h2>
-            <!-- TODO catalogue -->
-        HTML;
+        
+        if (empty($_SESSION['Series'])) {
+            return "<p>Aucune Series disponible. Créez-en une d'abord</p>";
+        }
+        
+          foreach ($_SESSION['Series'] as $serie) {
+
+            if ($serie instanceof Serie) {
+                $renderer = new AudioListRender($serie);
+                $res .= "<div class='serie-block'>";
+                $res .= $renderer->render();
+               
+              
+            } else {
+                $res .= "<p>Erreur : élément non valide dans la session.</p>";
+            }
+        }
+
+        return $res;
+
     }
 
     public function POST(): string {
         return $this->GET();
+
     }
 }
