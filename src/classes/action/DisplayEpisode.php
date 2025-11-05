53399f718bf1289dd9423da724eb2e8d0c2600c0
Yanis Husser <yanis.husser2@etu.univ-lorraine.fr>
2025-11-05 11:15:10 +0100

    ajout de display episode non final

---
+++ src/classes/action/DisplayEpisode.php
@@ -0,0 +1,45 @@
+<?php
+namespace iutnc\netvod\action;
+
+use iutnc\netvod\repository\NetvodRepository;
+
+class DisplayEpisode extends Action {
+
+    public function GET(): string {
+        $serieId = $_GET['serie'];
+        $episodeId = $_GET['episode'];
+
+        $repo = NetvodRepository::getInstance();
+        $episode = $repo->getEpisode($serieId, $episodeId);
+
+        // Affichage de l'épisode
+        $episodeDetails = <<<END
+        <h3>Épisode: {$episode['titre']}</h3>
+        <p>{$episode['description']}</p>
+        <video controls>
+            <source src="{$episode['video_url']}" type="video/mp4">
+            Votre navigateur ne supporte pas la vidéo.
+        </video><br><br>
+        END;
+
+        // Formulaire pour ajouter un commentaire et une note
+        $commentForm = <<<END
+        <form method='post' action='?action=add-commentary-note'>
+            <input type='hidden' name='serie' value='$serieId'>
+            <input type='hidden' name='episode' value='$episodeId'>
+        
+            Note : <input type='number' name='note' min="1" max="5"><br><br>
+            Commentaire : <br>
+            <textarea name='commentaire' rows='4' cols='50'></textarea><br><br>
+        
+            <button type='submit'>Valider</button>
+        </form>
+        END;
+
+        // Retourne l'épisode suivi du formulaire de commentaire
+        return $episodeDetails . $commentForm;
+    }
+    public function POST(): string {
+        return $this->GET();
+    }
+}
